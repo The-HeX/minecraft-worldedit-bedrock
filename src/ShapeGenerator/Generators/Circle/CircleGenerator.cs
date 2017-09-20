19d0654ebda9bf8a5f05a6be31a5817a032cc0e5
Eric Hexter <eric.hexter@gmail.com>
2017-09-20 08:24:08 -0500

    more

--- src/ShapeGenerator/Generators/Circle/CircleGenerator.cs
+++ src/ShapeGenerator/Generators/Circle/CircleGenerator.cs
@@ -14,7 +14,7 @@ namespace ShapeGenerator.Generators
             var centerX = options.CenterX;
             var centerZ = options.CenterZ;
             var fill = options.Fill;
-            var y = options.Y;
+            //var y = options.Y;
 
             var lowerX = centerX - radius - 1;
             var lowerZ = centerZ - radius - 1;
@@ -23,6 +23,7 @@ namespace ShapeGenerator.Generators
 
             for (var x = lowerX; x < upperX; x++)
             for (var z = lowerZ; z < upperZ; z++)
+            for(var y = options.Y;y<=options.Y+options.Height;y++)
             {
                 var distance = Distance(centerX, centerZ, x, z);
 
@@ -38,36 +39,9 @@ namespace ShapeGenerator.Generators
 
         public List<Line> TransformToLines(List<Point> points, Options options)
         {
-            return SimpleLinesFromPoints(points, options);
+            return SphereGenerator.LinesFromPoints(points, options);
         }
-
-        private static List<Line> SimpleLinesFromPoints(List<Point> points, Options opt)
-        {
-            var options = (ICircleOptions) opt;
-            var lines = new List<Line>();
-            points = points.OrderBy(a => a.X).ThenBy(a => a.Z).ToList();
-            var item = new Line {Start = points[0], End = points[0], Block = opt.Block};
-            item.Start.Y = options.CenterY;
-            item.End.Y = options.CenterY + options.Height;
-            lines.Add(item);
-            foreach (var point in points.Skip(1).ToList())
-            {
-                var lastLine = lines.Last();
-                if (point.X == lastLine.Start.X)
-                    if (point.Z == lastLine.End.Z + 1)
-                    {
-                        lastLine.End = point;
-                        lastLine.End.Y = options.CenterY + options.Height;
-                        continue;
-                    }
-                var item1 = new Line {Start = point, End = point, Block = opt.Block};
-                item1.Start.Y = options.CenterY;
-                item1.End.Y = options.CenterY + options.Height;
-                lines.Add(item1);
-            }
-
-            return lines;
-        }
+     
 
 
         private static double Distance(int centerX, int centerZ, int x, int z)
--- src/ShapeGenerator/Program.cs
+++ src/ShapeGenerator/Program.cs
@@ -11,35 +11,42 @@ namespace ShapeGenerator
     {
         private static void Main(string[] args)
         {
-            var _factory = new GeneratorFactory();
-            var time = DateTime.Now.ToString("hhmmssffff");
-            var options = new Options();
-            var isValid = Parser.Default.ParseArgumentsStrict(args, options);
-            if (!isValid)
-            {
-                Console.WriteLine("invalid arguments");
-                Console.Read();
-                return;
-            }
+            //try
+            //{
+                var _factory = new GeneratorFactory();
+                var time = DateTime.Now.ToString("hhmmssffff");
+                var options = new Options();
+                var isValid = Parser.Default.ParseArgumentsStrict(args, options);
+                if (!isValid)
+                {
+                    Console.WriteLine("invalid arguments");
+                    Console.Read();
+                    return;
+                }
 
-            var points = new List<Point>();
-            var lines = new List<Line>();
+                var points = new List<Point>();
+                var lines = new List<Line>();
 
-            var generator = _factory.Create(options.Shape);
-            points = generator.Run(options);
-            lines = generator.TransformToLines(points, options);
-
-
-            if (lines.Count > 0)
-                ExportService.WriteLinesFile(lines, $@"..\..\..\..\lines-{options.Shape}-{time}.csv");
-            if (points.Count > 0)
-                ExportService.WritePointsFile(points, $@"points-{options.Shape}-{time}.csv");
+                var generator = _factory.Create(options.Shape);
+                points = generator.Run(options);
+                lines = generator.TransformToLines(points, options);
 
 
-            Console.WriteLine($"there were {lines.Count} lines");
-            Console.WriteLine($"there were {points.Count} points");
-            //   Console.ReadKey();
-            Thread.Sleep(1000);
+                if (lines.Count > 0)
+                    ExportService.WriteLinesFile(lines, $@"..\..\..\..\lines-{options.Shape}-{time}.csv");
+                if (points.Count > 0)
+                    ExportService.WritePointsFile(points, $@"points-{options.Shape}-{time}.csv");
+
+
+                Console.WriteLine($"there were {lines.Count} lines");
+                Console.WriteLine($"there were {points.Count} points");
+                //   Console.ReadKey();
+                Thread.Sleep(1000);
+            //}catch(Exception e)
+            //{
+            //    Console.WriteLine(e);
+            //      Console.ReadKey();
+            //}
         }
     }
 }
